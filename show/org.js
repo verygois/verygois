1a68a6e898b4c2fff1531d40b24de50115afdd3a
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-20 01:20:38 +0900

    show

--- show/org.js
+++ show/org.js
@@ -36,4 +36,32 @@ function objORG(obj) {
   }
 }
 
-indexJSON();
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === 'interactive') {
+    indexJSON();
+  } else if (event.target.readyState === 'complete') {
+    let filter = document.querySelectorAll('#org input[type="radio"]');
+    //****** for all select ******
+    for (let i of filter) {
+      i.addEventListener('change', () => {
+        let value = i.value;
+        //*** for each target ***
+        let targets = document.querySelectorAll("#org a");
+        for (let ii of targets) {
+          //*** delete hidden ***
+          ii.style.opacity = "1";
+          ii.style.pointerEvents = "auto";
+          ii.style.userSelect = "auto";
+          //*** check target every select ***
+          let item_data = ii.getAttribute('class')
+          //*** set hidden ***
+          if (value && value !== 'all' && value !== item_data) {
+            ii.style.opacity = "0";
+            ii.style.pointerEvents = "none";
+            ii.style.userSelect = "none";
+          }
+        }
+      })
+    }
+  }
+});
