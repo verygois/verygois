ebb14e315a121519659dd7180f09cf9482b167d4
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-19 22:56:47 +0900

    show

--- show/premium-showcase/script.js
+++ show/premium-showcase/script.js
@@ -1,13 +1,28 @@
 'use strict'
 
-const allItems = document.querySelectorAll('.list li'),
-  date = document.querySelector("#date"),
-  img = document.querySelector("#img"),
-  text = document.querySelector("#text");
+const dialogModal = document.querySelector('#modal');
+const openModal = document.querySelector('#openModal');
 
+function onModal() {
+  if (typeof dialogModal.showModal === "function") {
+    dialogModal.showModal();
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+}
 
+const closeButton = document.querySelector('#closeButton');
+closeButton.addEventListener('click', () => {
+  dialogModal.close();
+});
+
+const allItems = document.querySelectorAll('.list li');
 for (const eachItem of allItems) {
   eachItem.addEventListener('click', function () {
-    eachItem.innerHTML = `<small class="log">${eachItem.dataset.info}</small>`;
+    const who = document.querySelector("#modal h4");
+    const about = document.querySelector("#modal small");
+    who.textContent = eachItem.textContent;
+    about.innerHTML = eachItem.dataset.info;
+    onModal()
   });
 }
