3951bce1a0c5beab7fc55b9756ecbfc84eb3c1ec
verygois <sasajimakazuma@gmail.com>
2024-04-15 18:39:11 +0900

    vg

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 'use strict'
 
-function indexBody() {
-  const lastModif = document.querySelector('#lastModified')
-  lastModif.textContent = new Date(document.lastModified)
-  lastModif.setAttribute("data-time", document.lastModified)
+const lastModif = document.querySelector('#lastModified')
+lastModif.textContent = new Date(document.lastModified)
+lastModif.setAttribute("data-time", document.lastModified)
 
-  lastModif.addEventListener('click', function (event) {
-    let ago = new Date(document.lastModified)
-    let diff = new Date().getTime() - ago.getTime();
+lastModif.addEventListener('click', function (event) {
+  let ago = new Date(document.lastModified)
+  let diff = new Date().getTime() - ago.getTime();
 
-    let progress = new Date(diff)
-    if (progress.getUTCFullYear() - 1970) {
-      event.target.textContent = progress.getUTCFullYear() - 1970 + ' year ago';
-    } else if (progress.getUTCMonth()) {
-      event.target.textContent = progress.getUTCMonth() + ' month ago';
-    } else if (progress.getUTCDate() - 1) {
-      event.target.textContent = progress.getUTCDate() - 1 + ' day ago';
-    } else if (progress.getUTCHours()) {
-      event.target.textContent = progress.getUTCHours() + ' hour ago';
-    } else if (progress.getUTCMinutes()) {
-      event.target.textContent = progress.getUTCMinutes() + ' minute ago';
-    } else {
-      event.target.textContent = 'Now';
-    }
-  }, false)
-}
+  let progress = new Date(diff)
+  if (progress.getUTCFullYear() - 1970) {
+    event.target.textContent = progress.getUTCFullYear() - 1970 + ' year ago';
+  } else if (progress.getUTCMonth()) {
+    event.target.textContent = progress.getUTCMonth() + ' month ago';
+  } else if (progress.getUTCDate() - 1) {
+    event.target.textContent = progress.getUTCDate() - 1 + ' day ago';
+  } else if (progress.getUTCHours()) {
+    event.target.textContent = progress.getUTCHours() + ' hour ago';
+  } else if (progress.getUTCMinutes()) {
+    event.target.textContent = progress.getUTCMinutes() + ' minute ago';
+  } else {
+    event.target.textContent = 'Now';
+  }
+}, false)
 
 async function fetchHTML(url = '', query = '') {
   fetch(url)
